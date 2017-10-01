01239e6c2b6dadc695dd90fdceb017cad3c1d90b
DESKTOP-1S1G3PH\Oleksandr <ahaidukov@intersog.com>
2017-10-01 13:21:56 +0300

    [ADD] app cam change camera possition

--- app/src/main/java/com/ohaidukov/punctualfrankie/punctualfrankie/MainScreenActivity.java
+++ app/src/main/java/com/ohaidukov/punctualfrankie/punctualfrankie/MainScreenActivity.java
@@ -2,18 +2,25 @@ package com.ohaidukov.punctualfrankie.punctualfrankie;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainScreenActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private SupportMapFragment mapFragment;
+public static String TAG = "ATATA";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +29,7 @@ public class MainScreenActivity extends FragmentActivity implements OnMapReadyCa
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -42,5 +50,68 @@ public class MainScreenActivity extends FragmentActivity implements OnMapReadyCa
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        UiSettings settings = mMap.getUiSettings();
+        settings.setAllGesturesEnabled(true);
+        settings.setCompassEnabled(true);
+        settings.setMyLocationButtonEnabled(true);
+        settings.setRotateGesturesEnabled(true);
+        settings.setScrollGesturesEnabled(true);
+        settings.setTiltGesturesEnabled(true);//смена угла обзора
+        settings.setZoomControlsEnabled(true);
+        settings.setZoomGesturesEnabled(true);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Log.d(TAG, "onMapClick: " + latLng.latitude + "," + latLng.longitude);
+            }
+        });
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                Log.d(TAG, "onMapLongClick: " + latLng.latitude + "," + latLng.longitude);
+            }
+        });
+
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+
+            @Override
+            public void onCameraChange(CameraPosition camera) {
+                Log.d(TAG, "onCameraChange: " + camera.target.latitude + "," + camera.target.longitude);
+                Log.d(TAG, "onCameraChange:  bearing=" + camera.bearing + ", tilt=" + camera.tilt+ ", zoom=" + camera.zoom);
+            }
+        });
+    }
+
+    public void onClickTest(View view) {
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(-27, 133))
+                .zoom(5)
+                .bearing(45)
+                .tilt(20)
+                .build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        mMap.animateCamera(cameraUpdate);
+    }
+
+    public void onClickMove(View view) {
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(new LatLng(
+                -27, 133));
+        //scrollBy (float xPixel, float yPixel)
+        //zoomBy (float amount)
+        //zoomBy (float amount, Point focus)
+        //zoomIn/zoomOut
+        //zoomTo (float zoom)
+        mMap.animateCamera(cameraUpdate);
+    }
+
+    public void onClickShowArea(View view) {
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(
+                new LatLngBounds(new LatLng(-39, 112), new LatLng(-11, 154)),
+                100);
+        mMap.animateCamera(cameraUpdate);
     }
 }
